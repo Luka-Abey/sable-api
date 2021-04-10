24be1504ebc5a7c3a6f8da1e9cb65ca3b875a8b9
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-10 23:48:34 +0100

    configs to allow eroku db

--- sableapi/settings.py
+++ sableapi/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'sableapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.path.join(BASE_DIR, "db.postgresql"))
     }
 }
 
