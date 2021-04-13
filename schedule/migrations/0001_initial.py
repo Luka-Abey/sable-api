89bfa8802cf6e6b3bfc07b503acf393812ba73ac
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-13 21:15:19 +0100

    trying to manually makemigrations

---
+++ schedule/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Show',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('date_time', models.DateTimeField()),
+                ('end_time', models.TimeField()),
+                ('live', models.BooleanField(default=True)),
+            ],
+        ),
+    ]
