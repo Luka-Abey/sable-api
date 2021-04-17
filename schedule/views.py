5baedc8a7b3d3a5e31d7900a9699769146112c02
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-17 14:20:00 +0100

    attemptint to get current show from time.now

--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.db.models import Q
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
