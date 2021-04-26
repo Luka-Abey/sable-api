1735dcee034f67b10c3dccfd9857ba9f9ae4a0c2
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-04-26 18:07:42 +0100

    something missing no resident found ...

--- residents/views.py
+++ residents/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
 from rest_framework.response import Response
@@ -20,6 +19,6 @@ class SingleResidentView(APIView):
   def get(self, request, *args, **kwargs):
     this_resident_name = request.query_params.get('id')
     print(this_resident_name)
-    this_resident = Resident.objects.get_object_or_404(id=this_resident_name)
+    this_resident = Resident.objects.get(id=this_resident_name)
     serializer = ResidentSerializer(this_resident, many=False)
     return Response(serializer.data)
