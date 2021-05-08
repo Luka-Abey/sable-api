2de1d15d8565936a400d8cc27df4585dbf0ae259
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-05-08 19:08:37 +0100

    create banner - add maxlengths

--- banner/migrations/0001_initial.py
+++ banner/migrations/0001_initial.py
@@ -13,9 +13,9 @@ class Migration(migrations.Migration):
             name='Banner',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(editable=True, blank=False)),
-                ('image_url', models.CharField(editable=True, blank=False)),
-                ('link_url', models.CharField(editable=True, blank=True)),
+                ('name', models.CharField(max_length=50, editable=True, blank=False)),
+                ('image_url', models.CharField(max_length=150, editable=True, blank=False)),
+                ('link_url', models.CharField(max_length=150, editable=True, blank=True)),
             ],
         ),
     ]
--- banner/models.py
+++ banner/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Banner(models.Model):
-  name = models.CharField(editable=True, blank=False)
-  image_url = models.CharField(editable=True, blank=False)
-  link_url = models.CharField(editable=True, blank=True)
+  name = models.CharField(max_length=50, editable=True, blank=False)
+  image_url = models.CharField(max_length=150, editable=True, blank=False)
+  link_url = models.CharField(max_length=150, editable=True, blank=True)
 
   def __str__(self):
     return self.name
