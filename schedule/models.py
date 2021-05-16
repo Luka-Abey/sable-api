04df0493a0a18258bc5968e72be8aec3b551acac
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-05-16 23:32:36 +0100

    attempt 2.2

--- schedule/models.py
+++ schedule/models.py
@@ -7,4 +7,5 @@ class Show(models.Model):
   live = models.BooleanField(default=True, editable=True)
 
   def __str__(self):
-    return f'{self.name} at {self.date_time.str}'
+    start_time = str(self.date_time)
+    return f'{self.name} at {start_time.strftime("%d/%m/%Y")}'
