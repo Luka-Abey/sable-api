2417ce2bbcfbc833b1fc28719d93b0e132d8cdba
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-06-05 23:52:16 +0100

    more user friendly?

--- residents/admin.py
+++ residents/admin.py
@@ -3,9 +3,15 @@ from django.db import models
 from .models import Resident
 from django import forms
 
+class ResidentAdminForm(forms.ModelForm):
+  class Meta:
+    model = Resident
+    widgets = {
+      'description': forms.Textarea
+    }
+    fields = '__all__'
+    
 class ResidentAdmin(admin.ModelAdmin):
-  formfield_overrides = {
-    models.CharField: {'widget': forms.Textarea}
-  }
+  form = ResidentAdminForm
 
 admin.site.register(Resident, ResidentAdmin)
